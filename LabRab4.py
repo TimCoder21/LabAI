f191234a0af8f3267531f0d049bef05e25b86450
user <timofei2005.com@gmail.com>
2025-11-22 00:00:07 +0300

    еще правка мелкая

--- LabRab4.py
+++ LabRab4.py
@@ -16,7 +16,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 rf_model = RandomForestClassifier(oob_score=True,random_state=42)
 rf_model.fit(X_train, y_train)
 y_pred_rf = rf_model.predict(X_test)
-y_pred_proba_rf = rf_model.predict_proba(X_test)
 print("Random Forest")
 print("OOB Score: ",rf_model.oob_score_)
 print("Test Accuracy: ",accuracy_score(y_test, y_pred_rf), '\n')
@@ -24,14 +23,12 @@ print("Test Accuracy: ",accuracy_score(y_test, y_pred_rf), '\n')
 ada_model = AdaBoostClassifier(random_state=42)
 ada_model.fit(X_train, y_train)
 y_pred_ada = ada_model.predict(X_test)
-y_pred_proba_ada = ada_model.predict_proba(X_test)[:, 1]
 print("AdaBoost")
 print("Test Accuracy: ", accuracy_score(y_test, y_pred_ada), '\n')
 
 gb_model = GradientBoostingClassifier(random_state=42)
 gb_model.fit(X_train, y_train)
 y_pred_gb = gb_model.predict(X_test)
-y_pred_proba_gb = gb_model.predict_proba(X_test)[:, 1]
 print("Gradient Boosting")
 print("Test Accuracy: ", accuracy_score(y_test, y_pred_gb))
 
@@ -43,7 +40,7 @@ RocCurveDisplay.from_estimator(gb_model, X_test, y_test, ax=ax, name='Gradient B
 plt.ylabel('TPR')
 plt.xlabel('FPR')
 
-ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Chance')
+ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r')
 
 ax.set_title('ROC-кривые')
 plt.show()
