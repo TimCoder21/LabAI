fc771aef302511b31cf869d6307573373a88d541
user <timofei2005.com@gmail.com>
2025-12-04 15:32:01 +0300

    поменял датасет для задачи классификации

--- LabRab2.py
+++ LabRab2.py
@@ -11,8 +11,8 @@ from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
 
-df = pd.read_csv("processed_titanic.csv")
 df2 = pd.read_csv("processed_apple.csv")
 
 X = df2[['Date']]
@@ -23,7 +23,7 @@ X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.4, r
 
 linear_model = LinearRegression()
 
-from sklearn.preprocessing import PolynomialFeatures
+
 n = 10
 poly_features = PolynomialFeatures(n)
 X_train = poly_features.fit_transform(X_train)
@@ -43,28 +43,20 @@ print("MAE: ", MAE)
 
 from ucimlrepo import fetch_ucirepo
 
-# fetch dataset
 spambase = fetch_ucirepo(id=94)
 
 # data (as pandas dataframes)
 X1 = spambase.data.features
 y1 = spambase.data.targets
 
-# metadata
+y1 = y1.values.ravel()
+
 print(spambase.metadata)
 
-# variable information
 print(spambase.variables)
-
-
-
-
-#X1 = df[['Age']]
-#y1 = df['Transported']
 
 X1_train, X1_test, y1_train, y1_test = train_test_split(X1, y1, test_size=0.4, random_state=42)
 X1_test, X1_val, y1_test, y1_val = train_test_split(X1_test, y1_test, test_size=0.4, random_state=42)
-
 
 logreg_model = LogisticRegression(max_iter=1000, random_state=42)
 logreg_model.fit(X1_train, y1_train)
@@ -72,8 +64,6 @@ y1_pred_test = logreg_model.predict(X1_test)
 
 accuracy = accuracy_score(y1_test, y1_pred_test)
 print("Точность: " , accuracy)
-
-
 
 cm = confusion_matrix(y1_test, y1_pred_test)
 print(cm)
