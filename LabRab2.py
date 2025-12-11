39f9cedb29c34fe8f0e2eff34756ebc1fab0ee06
user <timofei2005.com@gmail.com>
2025-12-11 12:26:35 +0300

    .

--- LabRab2.py
+++ LabRab2.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.metrics import root_mean_squared_error
 from sklearn.metrics import mean_absolute_error
@@ -9,19 +9,15 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.preprocessing import PolynomialFeatures
 
-df2 = pd.read_csv("processed_winequality-red.csv")
-
-X = df2.drop(['quality'], axis=1)
-y = df2['quality']
+df2 = pd.read_csv("processed_Fish.csv")
+X = df2.drop(['Weight'], axis=1)
+y = df2['Weight']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
-X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.4, random_state=42)
 
 linear_model = LinearRegression()
 linear_model.fit(X_train, y_train)
-
 y_pred_test = linear_model.predict(X_test)
 
 print("Оценка регресии")
@@ -40,8 +36,6 @@ spambase = fetch_ucirepo(id=94)
 # data (as pandas dataframes)
 X1 = spambase.data.features
 y1 = spambase.data.targets
-
-y1 = y1.values.ravel()
 
 print(spambase.metadata)
 print(spambase.variables)
