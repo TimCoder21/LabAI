e5f19b07cd191d18b8bd181b5982594e7275cada
user <timofei2005.com@gmail.com>
2025-12-22 16:47:34 +0300

    .

--- LabRab5.py
+++ LabRab5.py
@@ -16,24 +16,22 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 model_single_layer = keras.Sequential([
-    keras.layers.Dense(8, activation='sigmoid', input_shape=(12,)),
-    keras.layers.Dense(2, activation='softmax')
+   keras.layers.Dense(12, activation='relu', input_shape=(12,)),  # Скрытый слой 1
+   keras.layers.Dense(2, activation='sigmoid')                    # Выходной слой (бинарная классификация)
 ])
-
 model_single_layer.compile(
     optimizer='adam',
     loss='categorical_crossentropy',
     metrics=['accuracy']
 )
 
-history = model_single_layer.fit(X_train, y_train,epochs=100,batch_size=16,validation_data=(X_test, y_test),verbose=1)
+history = model_single_layer.fit(X_train, y_train,epochs=50,batch_size=16,validation_data=(X_test, y_test))
 
 test_loss, test_accuracy = model_single_layer.evaluate(X_test, y_test)
 print("\nТочность нейросети на тестовых данных: ", test_accuracy)
 
 y_pred_proba = model_single_layer.predict(X_test)
-y_pred = np.argmax(y_pred_proba, axis=1)
-y_test_classes = np.argmax(y_test, axis=1)
+
 
 plt.figure(figsize=(6, 4))
 
@@ -48,6 +46,8 @@ plt.grid(True, alpha=0.3)
 plt.tight_layout()
 plt.show()
 
-print("точность на тренировочных данных: ", history.history['accuracy'][-1])
 print("точность на тестовых данных: ", test_accuracy)
 
+y_pred_d = (model_single_layer.predict(X_test) > 0.5).astype("int32")
+
+print("Accuracy:", accuracy_score(y_test, y_pred_d))
