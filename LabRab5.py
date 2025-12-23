86871e94f41f0161a3602aba895b8a56823f00cf
user <timofei2005.com@gmail.com>
2025-12-23 13:37:17 +0300

    .

--- LabRab5.py
+++ LabRab5.py
@@ -1,53 +1,44 @@
 import numpy as np
+import tensorflow as tf
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
+from sklearn.datasets import make_moons
 import matplotlib.pyplot as plt
+
 
 np.random.seed(42)
 X = np.random.randint(0, 2, size=(100, 12))  # 100 примеров, 12 бинарных признаков
-Y = np.random.randint(0, 2, size=(100, 2))  # 100 примеров, 2 класса (one-hot encoding)
+y = np.random.randint(0, 2, size=(100, 2))  # 100 примеров, 2 класса (one-hot encoding)
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+# Разделяем на тренировочную и тестовую выборки
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
+# Нормализация данных
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-model_single_layer = keras.Sequential([
-   keras.layers.Dense(12, activation='relu', input_shape=(12,)),  # Скрытый слой 1
+model = keras.Sequential([
+   keras.layers.Dense(8, activation='relu', input_shape=(12,)),  # Скрытый слой 1
    keras.layers.Dense(2, activation='sigmoid')                    # Выходной слой (бинарная классификация)
 ])
-model_single_layer.compile(
-    optimizer='adam',
-    loss='categorical_crossentropy',
-    metrics=['accuracy']
-)
 
-history = model_single_layer.fit(X_train, y_train,epochs=50,batch_size=16,validation_data=(X_test, y_test))
+# Компиляция модели
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-test_loss, test_accuracy = model_single_layer.evaluate(X_test, y_test)
-print("\nТочность нейросети на тестовых данных: ", test_accuracy)
+# Обучение модели
+history = model.fit(X_train, y_train, epochs=50, batch_size=16, validation_data=(X_test, y_test))
 
-y_pred_proba = model_single_layer.predict(X_test)
+# Оцениваем качество на тестовой выборке
+y_pred = (model.predict(X_test) > 0.5).astype("int32")
+print("Accuracy:", accuracy_score(y_test, y_pred))
 
-
-plt.figure(figsize=(6, 4))
-
-plt.plot(history.history['loss'], label='Training Loss', linewidth=2)
-plt.plot(history.history['val_loss'], label='Validation Loss', linewidth=2)
+# График изменения функции ошибки
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
-plt.title('График функции ошибки')
 plt.legend()
-plt.grid(True, alpha=0.3)
-
-plt.tight_layout()
 plt.show()
-
-print("точность на тестовых данных: ", test_accuracy)
-
-y_pred_d = (model_single_layer.predict(X_test) > 0.5).astype("int32")
-
-print("Accuracy:", accuracy_score(y_test, y_pred_d))
